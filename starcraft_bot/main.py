b77d985d1c5544e8987a4a0dc4e9db1f0a606eab
mbresnan1701 <bresnan.mw@gmail.com>
2018-08-01 20:59:54 -0700

    builds workers, pylons, expands

--- starcraft_bot/main.py
+++ starcraft_bot/main.py
@@ -1,14 +1,50 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR
 
 class MattBot(sc2.BotAI):
     async def on_step(self, iteration):
         await self.distribute_workers()
+        await self.build_workers()
+        await self.build_pylons()
+        await self.build_assimilators()
+        await self.expand()
+
+    async def expand(self):
+        if self.units(NEXUS).amount < 3 and self.can_afford(NEXUS):
+            await self.expand_now()
+
+    async def build_assimilators(self):
+        for nexus in self.units(NEXUS).ready:
+            geysers = self.state.vespene_geyser.closer_than(15.0, nexus)
+            for geyser in geysers:
+                if not self.can_afford(ASSIMILATOR):
+                    break
+                worker = self.select_build_worker(geyser.position)
+                if worker is None:
+                    break
+
+                if not self.units(ASSIMILATOR).closer_than(1.0, geyser).exists:
+                    await self.do(worker.build(ASSIMILATOR, geyser))
+
+    async def build_workers(self):
+        for nexus in self.units(NEXUS).ready.noqueue:
+            if self.can_afford(PROBE):
+                await self.do(nexus.train(PROBE))
+
+    async def build_pylons(self):
+        if self.supply_left < 5 and not self.already_pending(PYLON):
+            nexii = self.units(NEXUS).ready
+            if nexii.exists:
+                if self.can_afford(PYLON):
+                    await self.build(PYLON, near=nexii.first)
+
+
 
 
 run_game(maps.get("AbyssalReefLE"), [
     Bot(Race.Protoss, MattBot()),
-    Computer(Race.Terran, Difficulty.Easy)
-], realtime=True)
+    Computer(Race.Zerg, Difficulty.Easy)
+], realtime=False)
 
